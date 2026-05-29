a905110c7d6ce2890c8c39f3b22e4e2c411de85d
hieuv9503-glitch <hieuv9503@gmail.com>
2026-05-29 23:52:24 +0700

    Update quanly.h

--- quanly.h
+++ quanly.h
@@ -3,19 +3,24 @@
 #include <iostream>
 #include <vector>
 #include "hoadon.h"
+#include "catruc.h"
 using namespace std;
 class QuanLy{
 private:
     vector<MonAn> dsMon;
     vector<HoaDon> dsHoaDon;
-
+    vector<catruc> dsCatructrongtuan;
 public:
-    void role();
-    void khoiTaoMenu();
-    void HienThiMenu();
-    void TaoHoaDon();
-    void xuatDanhSachHoaDon();
+    void role();//vai trò
+    void khoiTaoMenu();//tạo menu
+    void HienThiMenu();//hiển thị menu
+    void TaoHoaDon();//tạo hóa đơn
+    void xuatDanhSachHoaDon();//Xuất hóa đơn
     void menu();
-    void luuMenuVaoFile();
+    void luuMenuVaoFile();//tạo file để lưu vào cpu
+    void khoiTaoCacCaTruc();         // Tạo sẵn các ca Sáng/Chiều/Tối từ Thứ 2 đến CN
+    void nhanVienDangKyCa(string tenNV); // Hàm cho nhân viên chọn ca
+    void quanLySapXepCa();           // Hàm cho quản lý duyệt và sắp xếp
+    void hienThiLichLamViec();       // Hàm xem lịch trực chung
 };
 #endif
